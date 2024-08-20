5bbd32b9ce4377b8f9de5d1deef6802ea0b600c1
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-20 10:07:04 -0300

    feat: create CircuitCard component

---
+++ src/components/CircuitCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Button from "./Button.vue";
+</script>
+
+<template>
+  <div class="w-full bg-slate-100 p-4 rounded-md">
+    <div class="flex items-center justify-between">
+      <p class="text-lg font-semibold text-primary-900">
+        Título da situação problema
+      </p>
+      <p class="text-primary-700 font-medium">Estações: 5</p>
+    </div>
+    <p class="text-sm w-full mt-2">
+      Trecho do texto de contextualização especificado nesta situação problema
+      para permitir que o usuário entenda do que se trata sem necessitar
+      clicar...
+    </p>
+    <div class="mt-6 flex items-center justify-end w-full gap-4">
+      <button class="text-primary-800 hover:underline">Acessar</button>
+      <Button size="small" variant="outlined"> Aplicar </Button>
+    </div>
+  </div>
+</template>
